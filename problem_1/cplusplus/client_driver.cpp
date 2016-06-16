2328f33b5fb9617804131a0dcaa663cd792a5433
W. B. Metzel <w.metzel@gmail.com>
2016-06-16 12:24:55 -0400

    Setup initial files structure

---
+++ problem_1/cplusplus/client_driver.cpp
@@ -0,0 +1,20 @@
+/*******************************************************************************
+*
+* File: client_driver.cpp
+* Author: W. B. Metzel
+* Created: 16 June 2016
+* Last Revised: 16 June 2016
+* Description: The main program for Project Euler - problem one
+*
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+	int 0;
+	
+	std::cout << "Ha!" << std::endl;
+	
+	return 0;
+}
